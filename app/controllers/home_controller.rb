8bfe2c236653211f7352cf728f87e834f77d5bef
br0nc080 <mwelbu2@gmail.com>
2011-05-12 21:49:20 -0500

    more updates!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   require "instagram"
 
-  CALLBACK_URL = "http://www.michaelwelburn.com:3000"
+  CALLBACK_URL = "http://localhost:3000"
   CLIENT_ID = "b91ac9d30d814251b62dfa87df40b356"
   CLIENT_SECRET = "fd6595543bdc411a921002cf0f67bb66"
 
@@ -11,16 +11,24 @@ class HomeController < ApplicationController
   end
   
   def index
+    client = Instagram.client
+    @media = client.media_popular
+    #render :text => @media
+  end
+
+  def user
     if (params[:code])
       response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
       #response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL, :client_id => CLIENT_ID, :client_secret => CLIENT_SECRET, :grant_type => "authorization_code")
       session[:access_token] = response.access_token
-      redirect_to :root
+      redirect_to user_path
     elsif (session[:access_token])
       client = Instagram.client(:access_token => session[:access_token])
       user = client.user
 
-      render :text => user
+      #render :text => client.user_recent_media(user.id)
+      @media = client.user_recent_media(user.id)
+      #render :text => @media.first
     else
       redirect_to Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
     end
